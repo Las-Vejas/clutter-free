c7dfe266b363a3db9e37195c42e5b19ec4e0a93c
vejas <118309632+Las-Vejas@users.noreply.github.com>
2025-09-16 09:49:15 +0000

    idek

--- clutter-free/src/App.jsx
+++ clutter-free/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
+      return (
+    <>
+      <h1 className='h1'>About</h1>
+      <p>Hello there, my name is Vejas.
+        <br/>
+        This will be an inventory tracker app, but you'll have to wait...
+      </p>
+    </>
+  );
 }
+
 
 export default App
